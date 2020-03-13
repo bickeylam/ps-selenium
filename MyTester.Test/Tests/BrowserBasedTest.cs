df530769417e325fca9ad6efef5277ae13148c7f
bickey <bickey@hotmail.com>
2020-03-13 18:58:42 +1000

    initial commit

---
+++ MyTester.Test/Tests/BrowserBasedTest.cs
@@ -0,0 +1,67 @@
+namespace MyTester.Test
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Reflection;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+
+    [TestClass]
+    public class BrowserBasedTest
+    {
+        private readonly string DriverPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        private readonly string BaseUrl = @"http://www.google.com";
+        private readonly TimeSpan DefaultWait = TimeSpan.FromSeconds(10);
+        private IWebDriver driver;
+        public TestContext TestContext { get; set; }
+
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            driver = LoadDriver();
+            driver.Navigate().GoToUrl(BaseUrl);
+        }
+
+        [TestCleanup]
+        public void AfterTest()
+        {
+            driver?.Manage().Cookies.DeleteAllCookies();
+            driver?.Close();
+            driver?.Quit();
+        }
+
+        [TestMethod]
+        public void GivenSeleniumWhenSearchThenPageTitleVerified()
+        {
+            string term = "Selenium";
+            string expected = "Selenium - Google Search";
+
+            IWebElement searchText = driver.FindElement(By.Name("q"));
+            searchText.SendKeys(term);
+            searchText.Submit();
+
+            string actual = driver.Title;
+            Assert.AreEqual<string>(expected, actual);
+            TestContext.WriteLine($@"PageTitle: {actual}");
+            Debug.WriteLine($@"Debug: {actual}");
+            Trace.WriteLine($@"Trace: {actual}");
+        }
+
+        private IWebDriver LoadDriver()
+        {
+            ChromeOptions options = new ChromeOptions
+            {
+                AcceptInsecureCertificates = true
+            };
+            IWebDriver driver = new ChromeDriver(DriverPath, options);
+            driver.Manage().Timeouts().AsynchronousJavaScript = DefaultWait;
+            driver.Manage().Timeouts().PageLoad = DefaultWait;
+            driver.Manage().Timeouts().ImplicitWait = DefaultWait;
+            driver.Manage().Cookies.DeleteAllCookies();
+            driver.Manage().Window.Maximize();
+            return driver;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ps-selenium
+
 Selenium in Powershell
+
+## Commands
+
+```code
+dotnet test -l trx -c Release --filter TestCategory=TestCategoryName
+dotnet test -l trx --filter ClassName=ClassName
+
+dotnet publish <TestProject> -o <Destination>
+
+dotnet clean -c Release
+dotnet clean
+```
